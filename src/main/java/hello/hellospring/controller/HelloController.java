c294bf75895f1853bbf6d747afd0d4dbb95d903d
jjyjeh <jjyjeh@daum.net>
2024-01-14 17:37:53 +0900

    로컬브랜치 최초커밋

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,5 +1,7 @@
 package hello.hellospring.controller;
 
+import java.util.HashMap;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,9 @@ public class HelloController {
 	@GetMapping("hello")
 	public String hello(Model model) {
 		model.addAttribute("data", "hello!!");
+		HashMap<Object, String> input = new HashMap<Object, String>();
+		input.put("dtlYn", "Y");
+		
 		return "hello";
 	}
 
