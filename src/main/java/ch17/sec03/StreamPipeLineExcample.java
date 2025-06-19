5b82fa9adcf724d0bf2f2528103094a9116476b3
jjyjeh <jjyjeh@daum.net>
2025-06-19 13:48:24 +0900

    ch17_sec03

---
+++ src/main/java/ch17/sec03/StreamPipeLineExcample.java
@@ -0,0 +1,29 @@
+package ch17.sec03;
+
+import java.util.List;
+
+public class StreamPipeLineExcample {
+    public static void main(String[] args) {
+        List<Student> list = List.of(
+                new Student("홍길동", 10),
+                new Student("김길동", 20),
+                new Student("서길동", 30)
+        );
+
+        double avg = list.stream()
+                .mapToInt(Student -> Student.getScore())
+                .average().getAsDouble();
+
+        int tot = list.stream()
+                .mapToInt(student -> student.getScore())
+                .sum();
+
+        int max = list.stream().mapToInt(student -> student.getScore()).max().getAsInt();
+
+        System.out.println("평균점수 :: " + avg);
+        System.out.println("총점수 :: " + tot);
+        System.out.println("최고점수 :: " + max);
+
+        list.stream().map(student->student.getName()).forEach(name->System.out.println(name));
+    }
+}
